b13db0fdf02dbb29a853de15c18fc8fea21a233d
dripiki <dripiki@mail.ru>
2022-05-26 17:11:36 -0700

    merge: use shift

---
+++ branching/merge.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# display command line options
+
+count=1
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
+    count=$(( $count + 1 ))
+done
+#!/bin/bash
+# display command line options
+
+count=1
+while [[ -n "$1" ]]; do
+    echo "Parameter #$count = $1"
+    count=$(( $count + 1 ))
+    shift
+done
